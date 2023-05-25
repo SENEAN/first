3856bbd28202ae519dfcb422627c30b245d3aa77
SENEAN <0onmia0@gmail.com>
2023-05-25 10:09:29 +0900

    첫 문장에 작업

--- vcs_est/src/vcs_est/Hello.java
+++ vcs_est/src/vcs_est/Hello.java
@@ -1,8 +1,8 @@
 package vcs_est;
 
 public class Hello {
-
 	public static void main(String[] args) {
+		//로컬에서 작업했습니다.
 		System.out.println("Hello World");
 		
 		System.out.println("변경");
@@ -10,6 +10,8 @@ public class Hello {
 		System.out.println("세번째커밋");
 		
 		//깃으로도 간단한 수정은 가능하다, 다만 메모장수준의 수정인듯함
+		
+		
 
 	}
 
